d28bcfc1956bf12aa5ff998a9ba4d9c7b373aa1a
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-04-28 15:17:10 +0100

    adds styling

--- views/view.js
+++ views/view.js
@@ -9,15 +9,17 @@ View.prototype.viewStories = function() {
   for (i = 0; i < this.stories.length; i++) {
     storyText.push('<a href=#' + i + ">" + this.stories[i].webTitle + "</a></div><div><img src=" + this.stories[i].fields.thumbnail + "><div>" + this.storySummary(i));
   }
-  return "<ul><li><div>" + storyText.join("</div></li><li><div>") + "</a></div></li></ul>";
+  return "<ul><div><li><div>" + storyText.join("</div></li></div><div><li><div>") + "</a></div></li></div></ul>";
 };
 
 View.prototype.storySummary = function(index) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", this.url + this.stories[index].webUrl, false);
-  xhr.send();
-  this.AylienSummary = JSON.parse(xhr.response);
-  return this.AylienSummary.sentences;
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("GET", this.url + this.stories[index].webUrl, false);
+//   xhr.send();
+//   this.AylienSummary = JSON.parse(xhr.response);
+//   return this.AylienSummary.sentences;
+    return "So, there's a man crawling through the desert. He'd decided to try his SUV in a little bit of cross-country travel, had great fun zooming over the badlands and through the sand, got lost, hit a big rock, and then he couldn't get it started again. There were no cell phone towers anywhere near, so his cell phone was useless. He had no family, his parents had died a few years before in an auto accident, and his few friends had no idea he was out here. He stayed with the car for a day or so, but his one bottle of water ran out and he was getting thirsty. He thought maybe he knew the direction back, now that he'd paid attention to the sun and thought he'd figured out which way was north, so he decided to start walking. He figured he only had to go about 30 miles or so and he'd be back to the small town he'd gotten gas in last."
+
 };
 
   exports.View = View;
