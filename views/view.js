4fe127aa2346b5d5c095d12985e0cb2785746ad7
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-04-28 12:13:31 +0100

    adds full article page

--- views/view.js
+++ views/view.js
@@ -6,9 +6,9 @@
 View.prototype.viewStories = function() {
   var storyText = [];
   for (i = 0; i < this.stories.length; i++) {
-    storyText.push('<a href=#' + i + ">" + this.stories[i].webTitle + "</a></div><div><img src=" + this.stories[i].fields.thumbnail);
+    storyText.push('<a href=#' + i + ">" + this.stories[i].webTitle + "</a></div><div><img src=" + this.stories[i].fields.thumbnail + "><div>" + this.stories[i].blocks.body["0"].elements["0"].textTypeData.html );
   }
-  return "<ul><li><div>" + storyText.join("></div></li><li><div>") + "</a></div></li></ul>";
+  return "<ul><li><div>" + storyText.join("</div></li><li><div>") + "</a></div></li></ul>";
 };
   exports.View = View;
 })(this);
