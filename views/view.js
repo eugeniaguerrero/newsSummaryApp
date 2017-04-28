c5d3cf3b1805f3057a411b20b4cd2d2c1f8df6cf
Simba14 <seanblundell@gmail.com>
2017-04-28 12:34:00 +0100

    add summary of article using Aylien API

--- views/view.js
+++ views/view.js
@@ -1,14 +1,27 @@
 (function(exports) {
   function View(stories) {
     this.stories = stories;
+    this.url = "http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=";
   }
 
 View.prototype.viewStories = function() {
   var storyText = [];
   for (i = 0; i < this.stories.length; i++) {
-    storyText.push('<a href=#' + i + ">" + this.stories[i].webTitle + "</a></div><div><img src=" + this.stories[i].fields.thumbnail + "><div>" + this.stories[i].blocks.body["0"].elements["0"].textTypeData.html );
+    storyText.push('<a href=#' + i + ">" + this.stories[i].webTitle + "</a></div><div><img src=" + this.stories[i].fields.thumbnail + "><div>" + this.storySummary(i));
   }
   return "<ul><li><div>" + storyText.join("</div></li><li><div>") + "</a></div></li></ul>";
 };
+
+View.prototype.storySummary = function(index) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", this.url + this.stories[index].webUrl, false);
+  xhr.send();
+  this.AylienSummary = JSON.parse(xhr.response);
+  return this.AylienSummary.sentences;
+};
+
   exports.View = View;
 })(this);
+
+
+//this.stories[i].blocks.body["0"].elements["0"].textTypeData.html
