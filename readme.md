b9a7e0590159334a7e76ff6d68c17659a474b05b
Eugenia Guerrero <angeleuge95@gmail.com>
2017-06-07 16:29:31 +0100

    adds readme and deployment information

---
+++ readme.md
@@ -0,0 +1,9 @@
+## News Summary
+![](./images/Header.jpg)
+A single page application built in JavaScript, CSS, and HTML.
+It uses the Guardian API to fetch news stories and the Aylien API to generate the summary for each story.
+
+### To use locally
+- Download or clone the repository: `git clone https://github.com/eugeniaguerrero/newsSummaryApp.git`
+- Change into the repository: `cd newsSummaryApp`
+- Copy the full path of the index.html file and paste into your browser.
--- views/view.js
+++ views/view.js
@@ -13,12 +13,13 @@ View.prototype.viewStories = function() {
 };
 
 View.prototype.storySummary = function(index) {
-//   var xhr = new XMLHttpRequest();
-//   xhr.open("GET", this.url + this.stories[index].webUrl, false);
-//   xhr.send();
-//   this.AylienSummary = JSON.parse(xhr.response);
-//   return this.AylienSummary.sentences;
-    return "<p>So, there's a man crawling through the desert. He'd decided to try his SUV in a little bit of cross-country travel, had great fun zooming over the badlands and through the sand, got lost, hit a big rock, and then he couldn't get it started again. There were no cell phone towers anywhere near, so his cell phone was useless. He had no family, his parents had died a few years before in an auto accident, and his few friends had no idea he was out here. He stayed with the car for a day or so, but his one bottle of water ran out and he was getting thirsty.</p>";
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", this.url + this.stories[index].webUrl, false);
+  xhr.send();
+  this.AylienSummary = JSON.parse(xhr.response);
+  return "<p>" + this.AylienSummary.sentences + "</p>";
+    // This text was used to mock the Aylien API call
+    // return "<p>So, there's a man crawling through the desert. He'd decided to try his SUV in a little bit of cross-country travel, had great fun zooming over the badlands and through the sand, got lost, hit a big rock, and then he couldn't get it started again. There were no cell phone towers anywhere near, so his cell phone was useless. He had no family, his parents had died a few years before in an auto accident, and his few friends had no idea he was out here. He stayed with the car for a day or so, but his one bottle of water ran out and he was getting thirsty.</p>";
 
 };
 
